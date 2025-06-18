aa72db2883cce4be475d208ac7a6f3a9a9e163e9
ankushsingh7997 <cu.16bcs2432@gmail.com>
2025-06-18 12:23:59 +0530

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import removeConsole from 'vite-plugin-remove-console'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),removeConsole()],
   define: {
     global: 'globalThis',
   },
